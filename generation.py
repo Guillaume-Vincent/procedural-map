e9023a08cff7c4de9e9845b2204f9938ee6b9716
Guillaume Vincent <g_vincen@etud.insa-toulouse.fr>
2020-03-08 23:04:07 +0100

    Modified code to be able to generate a map and a few towns

--- generation.py
+++ generation.py
@@ -1,9 +1,10 @@
 from world import World
+from random import randint
 
 
-shape = (1000, 1000)
-scale = 300.0
-octaves = 5
+shape = (1920, 1080)
+scale = 700
+octaves = 6
 persistence = 0.5
 lacunarity = 2.0
 waterLevel = -0.2
@@ -11,5 +12,18 @@ waterLevel = -0.2
 
 map = World(shape, scale, octaves, persistence, lacunarity, waterLevel)
 map.generateMap()
+
+townOctaves = 5
+townPersistence = 0.5
+townLacunarity = 1.5
+
+for i in range(5):
+    size = randint(20, 175)
+    townScale = size // 4
+    townShape = (size, size)
+    townStretch = randint(150, 250) / 1000
+    map.addTown(shape=townShape, scale=townScale,
+                octaves=townOctaves, persistence=townPersistence,
+                lacunarity=townLacunarity, stretch=townStretch)
 map.showMap()
-map.saveMap(fileName="examples/std.png")
+map.saveMap(fileName="towns/std.png")
