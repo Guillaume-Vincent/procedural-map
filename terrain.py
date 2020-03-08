dadae54f557b5acec03cbe39500a15fd15685e5f
Guillaume Vincent <g_vincen@etud.insa-toulouse.fr>
2020-03-08 23:02:56 +0100

    Added a few colors

--- terrain.py
+++ terrain.py
@@ -7,3 +7,9 @@ class Terrain():
     forest = (0, 100, 0)
     mountain = (139, 137, 137)
     snow = (255, 250, 250)
+    town = (128,0,0)
+    
+    pink = (255, 20, 147)
+
+    # List of terrains a town can be built on
+    buildableList = [beach, plain, forest]
