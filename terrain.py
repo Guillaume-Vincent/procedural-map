3fde1036888b129b7c81266bdddb73e046d45875
Guillaume Vincent <g_vincen@etud.insa-toulouse.fr>
2020-03-14 03:10:31 +0100

    Renamed water to lake and added pond terrain

--- terrain.py
+++ terrain.py
@@ -1,7 +1,8 @@
 class Terrain:
     deepWater = (65, 105, 225)
-    water = (0, 191, 255)
+    lake = (0, 191, 255)
     river = (59, 179, 208)
+    pond = (0, 128, 128)
     sand = (210, 180, 140)
     beach = (238, 214, 175)
     plain = (34, 139, 34)
