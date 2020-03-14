f5a3f3409f09c3f2a7650468208172e2b9338738
Guillaume Vincent <g_vincen@etud.insa-toulouse.fr>
2020-03-14 03:10:13 +0100

    Moved static methods outside of Perlin class and added list_neighbours function

--- perlin.py
+++ perlin.py
@@ -5,9 +5,41 @@
 -persistence: how much an octave contributes to the overall shape (amplitude).
 """
 
+from bresenham import bresenham
 from random import randint
 from noise import pnoise2
 from numpy import linspace, sqrt, zeros
+
+
+def gradient(len_x, len_y):
+    """Create a gradient of altitude."""
+    x_axis = linspace(-1, 1, len_x)[:, None]
+    y_axis = linspace(-1, 1, len_y)[None, :]
+
+    arr = sqrt((x_axis ** 2 + y_axis ** 2) / 2)
+    return arr
+
+
+def list_neighbours(loc_list: list):
+    """Return a list of the neighbours of the elements of a given list that are not already in the list."""
+    neigh_list = []
+    for loc in loc_list:
+        neigh_list += [loc_neighbours(loc)[n] for n in range(8) if loc_neighbours(loc)[n] not in loc_list + neigh_list]
+    return neigh_list
+
+
+def loc_neighbours(loc: tuple):
+    """Return a list of all the direct neighbours of the given point."""
+    neighbours_list = [(loc[0] - 1, loc[1] - 1), (loc[0] - 1, loc[1]), (loc[0] - 1, loc[1] + 1),
+                       (loc[0], loc[1] - 1), (loc[0], loc[1] + 1),
+                       (loc[0] + 1, loc[1] - 1), (loc[0] + 1, loc[1]), (loc[0] + 1, loc[1] + 1)]
+    return neighbours_list
+
+
+def straight_line(from_loc, to_loc):
+    """Return a list containing all the points from a to b in a straight line."""
+    return bresenham(from_loc[0], from_loc[1],
+                     to_loc[0], to_loc[1])
 
 
 class Perlin:
@@ -27,13 +59,8 @@ class Perlin:
         # Noise map
         self.noiseMap = zeros(self.shape)
 
-    def get_map(self):
-        """Return a randomly generated noise map."""
-        self.generate_noise()
-        return self.noiseMap
-
     def generate_noise(self):
-        """Generate a noise map"""
+        """Generate a noise map."""
         self.__clear()
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
@@ -53,8 +80,17 @@ class Perlin:
         """Clear the noise map"""
         self.noiseMap = zeros(self.shape)
 
+    def get_delta_h(self):
+        """Get the average variation of height in the noise map."""
+        delta_list = []
+        for i in range(15):
+            for j in range(15):
+                delta_list.append(abs(self.noiseMap[i][j] - self.noiseMap[i][j + 1]))
+                delta_list.append(abs(self.noiseMap[i][j] - self.noiseMap[i + 1][j]))
+        return sum(delta_list) / len(delta_list)
+
     def points_higher_than(self, min_value):
-        """Return a list of all the points of the noiseMap higher than minValue"""
+        """Return a list of all the points of the noiseMap higher than minValue."""
         points_list = []
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
@@ -62,22 +98,10 @@ class Perlin:
                     points_list.append((i, j))
         return points_list
 
-    @staticmethod
-    def neighbours(loc, dist=1):
-        """Return a list of all the points a certain distance away from loc"""
-        neighbours_list = []
-        for i in range(loc[0] - dist, loc[0] + dist + 1):
-            neighbours_list.append((i, loc[1] - dist))
-            neighbours_list.append((i, loc[1] + dist))
-        for j in range(loc[1] - dist + 1, loc[1] + dist):
-            neighbours_list.append((loc[0] - dist, j))
-            neighbours_list.append((loc[0] + dist, j))
-        return neighbours_list
-
-    @staticmethod
-    def gradient(len_x, len_y):
-        x_axis = linspace(-1, 1, len_x)[:, None]
-        y_axis = linspace(-1, 1, len_y)[None, :]
-
-        arr = sqrt((x_axis ** 2 + y_axis ** 2) / 2)
-        return arr
+    def points_lower_than(self, max_value):
+        points_list = []
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                if self.noiseMap[i][j] < max_value:
+                    points_list.append((i, j))
+        return points_list
